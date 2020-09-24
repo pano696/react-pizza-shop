1c7ee6e59fd6028e1bea655304da9b7a0e0e22fc
Pavel S. Murzagaliev <pano696@gmail.com>
2020-09-24 18:00:17 +0300

    Extract header button in Components

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export default function Button({className, outline, children, onClick}) {
+  return (
+    <button
+      onClick={onClick}
+      className={classNames('button', className, {
+        'button--outline': outline
+    })}>
+      {children}
+    </button>
+  )
+}
