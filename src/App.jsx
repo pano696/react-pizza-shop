b1ed9e50c7a023477006eb45758334e3426caf0b
Pavel S. Murzagaliev <pano696@gmail.com>
2020-10-06 11:33:59 +0300

    Add PizzaBlock

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Header} from "./components"
 import {Home, Cart} from "./pages"
 import { Route } from "react-router-dom";
@@ -7,13 +7,19 @@ import { Route } from "react-router-dom";
 
 const App = () => {
 
+  const [db, loadDb] = useState([]);
 
+  useEffect(() => {
+     fetch("http://localhost:3000/db.json")
+      .then(resp=>resp.json())
+      .then(resp=>loadDb(resp.pizzas));
+  }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' render={ () => <Home items={db} />} />
         <Route exact path='/cart' component={Cart} />
       </div>
     </div>
